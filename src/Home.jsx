deb0129a54c5f7d93971c6a7b8d821adab2d16d4
Omotolani Popoola <omotolanipopoola@DotPays-MacBook-Pro-2.local>
2022-09-26 15:55:25 +0100

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Home() {
+  return (
+    <>
+      <div className='w-full h-[65vh] text-center'>
+        <h3 className='mt-[100px] font-semibold text-5xl'>Welcome</h3>
+        <p className='mt-[30px] font-medium text-3xl'>to</p>
+        <h1 className='mt-[30px] font-extrabold text-7xl'>Beloved Thorlani</h1>
+        <p className='mt-[30px] font-medium text-3xl'>Home of Online</p>
+        <h4 className='mt-[30px] font-bold text-4xl'>Law past questions</h4>
+      </div>
+    </>
+  )
+}
